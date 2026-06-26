9bbfe7093dfa1ca40c7c8a73c2f8cb1c37fb64e3
Jeannette1704 <jeannette.titiana@2028.icam.fr>
2026-06-26 14:03:39 +0200

    Clean up Terraform configuration by removing unused parts
    Removed unused provider and resource configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,3 @@
-terraform {
-  required_providers {
-    render = {
-      source  = "render-oss/render"
-      version = "1.3.3"
-    }
-  }
-}
-
-provider "render" {
-  api_key  = var.render_api_key
-  owner_id = var.render_owner_id
-}
-
-variable "github_actor" {
-  type = string
-}
-
-variable "render_api_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "render_owner_id" {
-  type = string
-}
-
-variable "image_url" {
-  type = string
-}
-
-variable "image_tag" {
-  type = string
-}
-
 resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
@@ -48,18 +13,6 @@ resource "render_web_service" "flask_app" {
     image = {
       image_url = var.image_url
       tag       = var.image_tag
-    }
-  }
-}
-
-resource "render_web_service" "adminer" {
-  name   = "adminer-${var.github_actor}"
-  plan   = "free"
-  region = "frankfurt"
-
-  runtime_source = {
-    image = {
-      image_url = "adminer:latest"
     }
   }
 }
