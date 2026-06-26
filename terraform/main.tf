07d5d9f1639129a0e17b7c0ecc6b3a1e81060b9d
Jeannette1704 <jeannette.titiana@2028.icam.fr>
2026-06-26 13:59:34 +0200

    Configure render provider and update web service resources
    Added render provider configuration and new variables for API key and owner ID. Updated web service resources to use variable values for image URL and tag.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,30 @@ terraform {
   }
 }
 
-provider "render" {}
+provider "render" {
+  api_key  = var.render_api_key
+  owner_id = var.render_owner_id
+}
 
 variable "github_actor" {
-  type        = string
+  type = string
+}
+
+variable "render_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "render_owner_id" {
+  type = string
+}
+
+variable "image_url" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
 }
 
 resource "render_web_service" "flask_app" {
@@ -26,7 +46,20 @@ resource "render_web_service" "flask_app" {
 
   runtime_source = {
     image = {
-      image_url = "dummy_value_replaced_by_github_actions"
+      image_url = var.image_url
+      tag       = var.image_tag
+    }
+  }
+}
+
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+
+  runtime_source = {
+    image = {
+      image_url = "adminer:latest"
     }
   }
 }
